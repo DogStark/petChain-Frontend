649f638fabb0fae616231672cb446285e683f8b5
Otaiki1 <otaikisadiq2804@gmail.com>
2026-02-21 10:06:00 +0100

    feat: add stellar wallet mgmt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -25,6 +25,7 @@ import { CertificatesModule } from './modules/certificates/certificates.module';
 import { MedicalRecordsModule } from './modules/medical-records/medical-records.module';
 import { VetsModule } from './modules/vets/vets.module';
 import { EmergencyServicesModule } from './modules/emergency-services/emergency-services.module';
+import { AppointmentWaitlistModule } from './modules/appointment-waitlist/appointment-waitlist.module';
 import { SearchModule } from './modules/search/search.module';
 
 // File Upload & Storage Modules
@@ -37,6 +38,7 @@ import { CdnModule } from './modules/cdn/cdn.module';
 import { FilesModule } from './modules/files/files.module';
 import { RealtimeModule } from './modules/realtime/realtime.module';
 import { WalletsModule } from './modules/wallets/wallets.module';
+import { StellarWalletManagementModule } from './modules/stellar-wallet-management/stellar-wallet-management.module';
 import { EmailModule } from './modules/email/email.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
@@ -87,6 +89,7 @@ import { AnalyticsModule } from './modules/analytics/analytics.module';
     MedicalRecordsModule,
     VetsModule,
     EmergencyServicesModule,
+    AppointmentWaitlistModule,
     SearchModule,
 
     // File Upload, Storage, Security & Processing
@@ -99,6 +102,7 @@ import { AnalyticsModule } from './modules/analytics/analytics.module';
     FilesModule,
     RealtimeModule,
     WalletsModule,
+    StellarWalletManagementModule,
     // Email
     EmailModule,
     // Notifications
